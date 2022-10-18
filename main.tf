685d19caaf30db6cbcb0b7c9ce86e313ba3bbb1d
vineet <vineetj_29@yahoo.com>
2022-10-18 12:44:59 +0530

    remove commented part

--- main.tf
+++ main.tf
@@ -44,33 +44,3 @@ module "ecs" {
   name   = local.name
   tags   = var.tags
 }
-
-# module "ecs-services" {
-#   source                           = "./ecs-services"
-#   name                             = "flask-app"
-#   health_check_path                = "/"
-#   port                             = 5001
-#   host_header                      = each.value.host_header
-#   lb_443_listener_arn              = module.alb.alb_443_listener_arn
-#   cluster_arn                      = module.ecs.cluster_arn
-#   vpc_id                           = data.aws_vpc.default_vpc
-#   security_group                   = module.sg-association.security_group_id
-#   subnets                          = data.aws_subnet_ids.subnets
-#   minimum_healthy_percent          = var.minimum_healthy_percent
-#   maximum_healthy_percent          = var.maximum_healthy_percent
-#   target_group_port                = var.target_group_port
-#   target_group_protocol            = var.target_group_protocol
-#   target_type                      = var.target_type
-#   deregistration_delay             = var.deregistration_delay
-#   health_check_healthy_threshold   = var.health_check_healthy_threshold
-#   health_check_unhealthy_threshold = var.health_check_unhealthy_threshold
-#   health_check_timeout             = var.health_check_timeout
-#   health_check_interval            = var.health_check_interval
-#   health_check_matcher             = var.health_check_matcher
-#   health_check_protocol            = var.health_check_protocol
-#   tags                             = var.tags
-#   ecs_cluster_name                 = module.ecs.cluster_name
-#   maximum_count                    = var.maximum_count
-#   desired_count                    = var.desired_count
-#   minimum_count                    = var.minimum_count
-# }
